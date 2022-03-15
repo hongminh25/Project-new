6d7e98b8fb6141275214211cefc759a1320b82ed
HongMinh <dohongminh25@gmail.com>
2022-03-15 15:09:39 +0700

    update

---
+++ app/src/main/java/com/example/project/QuanLyNodeFragment.java
@@ -0,0 +1,21 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class QuanLyNodeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_quan_ly_node, container, false);
+    }
+}
