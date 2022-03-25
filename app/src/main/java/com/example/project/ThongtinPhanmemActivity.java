f3f8cc24d919c3938bec9645138fc99daccd8104
HongMinh <dohongminh25@gmail.com>
2022-03-25 22:23:02 +0700

    update

---
+++ app/src/main/java/com/example/project/ThongtinPhanmemActivity.java
@@ -0,0 +1,14 @@
+package com.example.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThongtinPhanmemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thongtin_phanmem);
+    }
+}
