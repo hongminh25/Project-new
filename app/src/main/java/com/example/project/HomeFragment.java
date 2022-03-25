a88cda700156ceb41554d332fc1d0f0e0888f652
HongMinh <dohongminh25@gmail.com>
2022-03-25 21:20:57 +0700

    update

--- app/src/main/java/com/example/project/HomeFragment.java
+++ app/src/main/java/com/example/project/HomeFragment.java
@@ -12,14 +12,23 @@ import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
+    Button btn_themNode;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_themNode = (Button) view.findViewById(R.id.btn_ThemNode);
+        btn_themNode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ThemNodeActivity.class);
+                startActivity(intent);
+            }
+        });
 
-
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
 }
